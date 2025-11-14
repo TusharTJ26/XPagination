856cb4a2e4e6c017a571a26c8a1491390ff788a7
Tushar Jain <tushar.26tj@gmail.com>
2025-11-14 16:15:22 +0530

    change3

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import Table from "./table/Table";
 
 function App() {
   const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       let response = await fetch(
+  //         "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+  //       );
+  //       let data = await response.json();
+  //       setData(data);
+  //     } catch (e) {
+  //       console.error("failed to fetch data");
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await fetch(
-          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-        );
-        let data = await response.json();
-        setData(data);
-      } catch (e) {
-        console.error("failed to fetch data");
-      }
-    };
-    fetchData();
+    fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error("Fetch failed");
+        return res.json();
+      })
+      .then((data) => setUsers(data))
+      .catch(() => {
+        alert("Failed to fetch data"); // EXACT TEXT REQUIRED
+      });
   }, []);
 
   return (
